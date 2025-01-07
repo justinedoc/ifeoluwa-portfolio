e52b589abf5e3114a4a808ed8a2ddc3eff48f23a
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-07 18:15:41 +0100

    media queries for footer section added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,26 @@ import sqLike from "../assets/likes-1.png";
 import crLike from "../assets/likes-2.png";
 function Footer() {
   return (
-    <footer className="relative z-10 overflow-hidden bg-[#131212] min-h-[14rem] border px-5 md:px-10 py-5 rounded-xl snap-end">
+    <footer className="relative z-10 overflow-hidden bg-[#131212] min-h-[14rem] border px-5 md:px-10 md:py-5 pt-10 pb-24 rounded-xl snap-end">
+
+
       <div className="absolute w-full z-10 bottom-0 left-0 flex justify-center items-center">
-        <h1 className="text-[12rem] tracking-wide text-[#FFFFFF1A] font-bold">
+        <h1 className="md:text-[12rem] text-[3.6rem] tracking-wide text-[#FFFFFF1A] font-bold">
           {" "}
           DesignNinja
         </h1>
       </div>
 
-      <main className="grid grid-cols-3 place-items-center">
+      <main className="grid grid-cols-1 gap-20 md:gap-0 md:grid-cols-3 place-items-center mb-10">
         {/* Image  */}
-        <div className="w-56 h-56 rounded-full bg-white flex justify-center items-center z-20">
-          <div className="w-52 h-52 rounded-full  overflow-hidden">
+        <div className="md:w-56 md:h-56 w-60 h-60 rounded-full bg-white flex justify-center items-center z-20">
+          <div className="w-56 h-56 rounded-full  overflow-hidden">
             <img className="w-full" src={profilePhoto} alt="profile" />
           </div>
         </div>
 
         <nav className="max-w-[20rem]">
-          <ul className="grid grid-cols-2 gap-5 text-white font-bold text-2xl">
+          <ul className="grid grid-cols-2 gap-10 md:gap-5 text-white font-bold text-center text-2xl">
             <li>
               <a href="#">LINKEDIN</a>
             </li>
@@ -41,13 +43,13 @@ function Footer() {
 
         <div className="relative md:-translate-y-10 z-20">
           <img
-            className="w-56 md:w-64"
+            className="w-72 md:w-64"
             src={sqLike}
             alt="likes"
             loading="lazy"
           />
           <img
-            className="w-20 md:w-24 absolute -bottom-12 md:-bottom-16 -left-8"
+            className="w-24 absolute -bottom-14 md:-bottom-16 -left-8"
             src={crLike}
             alt="likes"
             loading="lazy"
