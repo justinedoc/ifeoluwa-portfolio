9ad75b178fae25d929feb981f853216450d7dc9e
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-07 17:34:05 +0100

    media queries for works section added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,27 +2,30 @@
 import { useState } from "react";
 import Navbar from "../ui/Navbar";
 import { dataForProducts } from "../data/dataForProducts";
+import useScreenSize from "../hooks/useScreenSize";
 
 const navItems = ["Product Design", "Low - Codes", "Branding", "Illustrations"];
 function Works() {
   const [activeNav, setActiveNav] = useState(navItems[0]);
-
+  const { isSmallScreened } = useScreenSize();
   return (
-    <section className="px-10 __section-inner w-full">
-      <Navbar isInverted />
+    <section className="md:px-10 __section-inner w-full">
+      {!isSmallScreened && <Navbar isInverted />}
 
       {/* Header and Navigation */}
-      <div className="text-black my-5 flex justify-between items-baseline">
-        <h1 className="font-bold tracking-tight text-[3.5rem]">My Works</h1>
+      <div className="text-black my-2 md:my-5 flex flex-col md:flex-row md:justify-between md:items-baseline">
+        <h1 className="font-bold tracking-tight text-[3.5rem] text-center">
+          My Works
+        </h1>
 
         <nav>
-          <ul className="flex gap-3 items-center bg-[#C5DEFE] rounded-lg px-2 py-1">
+          <ul className="flex justify-between md:gap-3 items-center bg-[#C5DEFE] rounded-lg px-2 py-1 my-3 md:my-0">
             {navItems.map((sec) => (
               <li
                 key={sec}
                 onClick={() => setActiveNav(sec)}
                 role="button"
-                className={`px-3 py-[0.3rem] text-black font-bold text-sm cursor-pointer transition-all duration-300 ease border-2 border-transparent
+                className={`px-[0.3rem] py-2 md:px-3 md:py-[0.3rem] text-black text-xs font-bold md:text-sm cursor-pointer transition-all duration-300 ease border-2 border-transparent
                   ${
                     activeNav === sec
                       ? "bg-black text-white rounded-md -translate-y-3 border-white"
@@ -37,7 +40,7 @@ function Works() {
       </div>
 
       {/* FIXME: h-[30rem] overflow-auto */}
-      <section className="my-10 grid grid-cols-1 md:grid-cols-2 gap-14 ">
+      <section className="my-5 md:my-10 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-14 ">
         {dataForProducts.map((product, i) => (
           <ProductCard key={i} product={product} index={1 + i} />
         ))}
@@ -47,9 +50,10 @@ function Works() {
 }
 
 function ProductCard({ product, index }) {
-  const isEven = index % 2 === 0;
+  const { isSmallScreened } = useScreenSize();
+  const isEven = index % 2 === 0 && !isSmallScreened;
   return (
-    <main className={`${!isEven ? "translate-y-5" : ""}`}>
+    <main className={`${!isEven ? "md:translate-y-5" : ""}`}>
       {isEven && <Image isEven={isEven} productImg={product?.img} />}
 
       <div className="flex items-center justify-between">
